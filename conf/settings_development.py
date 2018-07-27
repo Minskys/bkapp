a1bfb57946ff952b7babe9195a28f174c0de5c0b
Minskys <39076739+Minskys@users.noreply.github.com>
2018-07-27 14:21:50 +0800

    Update settings_development.py

--- conf/settings_development.py
+++ conf/settings_development.py
@@ -20,8 +20,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
-        'USER': 'root',                        # 你的数据库user
-        'PASSWORD': '!Aa123456',                        # 你的数据库password
+        'USER': 'ljmin',                        # 你的数据库user
+        'PASSWORD': 'Aa123456',                        # 你的数据库password
         'HOST': 'localhost',                   # 开发的时候，使用localhost
         'PORT': '3306',                        # 默认3306
     },
