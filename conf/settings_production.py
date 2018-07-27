0e292d2c8a8f2454d4344c36d2e5cd19bc6a6bc5
Minskys <39076739+Minskys@users.noreply.github.com>
2018-07-27 14:20:36 +0800

    Update settings_production.py

--- conf/settings_production.py
+++ conf/settings_production.py
@@ -20,7 +20,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
-        'USER': 'root',                        # 你的数据库user
+        'USER': 'ljmin',                        # 你的数据库user
         'PASSWORD': '!Aa123456',                        # 你的数据库password
         'HOST': '10.10.0.133',                   # 开发的时候，使用localhost
         'PORT': '3306',                        # 默认3306
